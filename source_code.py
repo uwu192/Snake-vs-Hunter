be50915ab15c3b81f01d16aac72b26da3592dd92
Tama <tam685669@gmail.com>
2023-11-20 01:15:33 +0700

    Add banana

--- source_code.py
+++ source_code.py
@@ -12,12 +12,13 @@ height = 1300
 width = 700
 screen = pygame.display.set_mode((height,width))
 
+
 def main_screen():
-    logo = pygame.image.load("C:/Code/project/Snake and Hunter/assets/logo.png")
+    logo = pygame.image.load("assets/logo.png")
     pygame.display.set_icon(logo)
     pygame.display.set_caption("Snake vs Hunter ---By Tama")
 def message(msg,text_color,size,x,y):
-    font = pygame.font.Font("C:/Code/project/Snake and Hunter/assets/font.ttf", size)
+    font = pygame.font.Font("assets/font.ttf", size)
     text = font.render(msg,True,text_color)
     screen.blit(text ,(x , y))
 def main():
@@ -25,12 +26,14 @@ def main():
     pygame.display.init()
     main_screen()
     #Khởi tạo hình ảnh
-    background = pygame.image.load("C:/Code/project/Snake and Hunter/assets/background.png")
-    head_snake = pygame.image.load("C:/Code/project/Snake and Hunter/assets/head.png")
-    head_snake_stun = pygame.image.load("C:/Code/project/Snake and Hunter/assets/dead_stun.png")
-    apple = pygame.image.load("C:/Code/project/Snake and Hunter/assets/apple.png")
-    snake_body = pygame.image.load("C:/Code/project/Snake and Hunter/assets/snake_body.png")
+    background = pygame.image.load("assets/background.png")
+    head_snake = pygame.image.load("assets/head.png")
+    head_snake_stun = pygame.image.load("assets/dead_stun.png")
+    apple = pygame.image.load("assets/apple.png")
+    banana = pygame.image.load("assets/banana.png")
+    snake_body = pygame.image.load("assets/snake_body.png")
     apple = pygame.transform.scale(apple, (50,50))
+    banana = pygame.transform.scale(banana, (50,50))
     snake_body = pygame.transform.rotate(snake_body,90)
     head_snake_stun = pygame.transform.scale(head_snake_stun, (50,70))
     head_snake_stun = pygame.transform.rotate(head_snake_stun,90)
@@ -46,6 +49,10 @@ def main():
     apple_x = 200
     apple_y = 100
     time_drop_apple = 0
+    # Vị trí của trái chuối
+    banana_x = 200
+    banana_y = 100
+    time_drop_banana = 0
     # Số pixel con rắn đi được(tốc độ di chuyển)
     snake_step_x = 5
     snake_step_y = 0
@@ -53,6 +60,8 @@ def main():
     running = True
     game_over = False
     have_apple = True
+    have_banana = False
+    skill_snake = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -129,13 +138,39 @@ def main():
         #------------------------------------------------------------------------
         pygame.time.wait(30)
         screen.blit(background,(0,0))
+        #----------------------------------------------
+        #Xoay cơ thể-----------------------------------
+        pos = [snake_xpos,snake_ypos]
+        snake_list.insert(0,(list(pos)))
+        for body_part in range(len(snake_list)):
+            snake_body_x = snake_list[body_part][0]
+            snake_body_y = snake_list[body_part][1]
+            if rotate == "RIGHT":
+                screen.blit(snake_body,(snake_body_x - 40,snake_body_y))
+            elif rotate == "DOWN":
+                screen.blit(snake_body,(snake_body_x,snake_body_y - 40))
+            elif rotate == "UP":
+                screen.blit(snake_body,(snake_body_x,snake_body_y + 60))
+            elif rotate == "LEFT":
+                screen.blit(snake_body,(snake_body_x + 60,snake_body_y))
+        #----------------------------------------------
         #Game_over nếu rắn chết(vì đụng tường)-----------------------
-        print(snake_xpos)
         if (snake_xpos > 1200) or (snake_ypos > 600) or (snake_xpos < 30) or (snake_ypos < 30) :
             game_over = True
         while game_over == True:    
             message("Hunter Win",red,50,400,width/2)
             message("Press Q-Quit or C-Play Again",black,25,300,width/2-200)
+            for body_part in range(len(snake_list)):
+                snake_body_x = snake_list[body_part][0]
+                snake_body_y = snake_list[body_part][1]
+                if rotate == "RIGHT":
+                    screen.blit(snake_body,(snake_body_x - 40,snake_body_y))
+                elif rotate == "DOWN":
+                    screen.blit(snake_body,(snake_body_x,snake_body_y - 40))
+                elif rotate == "UP":
+                    screen.blit(snake_body,(snake_body_x,snake_body_y + 60))
+                elif rotate == "LEFT":
+                    screen.blit(snake_body,(snake_body_x + 60,snake_body_y))
             screen.blit(head_snake_stun, (snake_xpos,snake_ypos))
             pygame.display.update()
             for event in pygame.event.get():
@@ -149,22 +184,38 @@ def main():
                     if event.key == pygame.K_c:
                         main()
         #----------------------------------------------
-        #Xoay cơ thể-----------------------------------
-        pos = [snake_xpos,snake_ypos]
-        snake_list.insert(0,(list(pos)))
-        print(snake_list)
-        for body_part in range(len(snake_list)):
-            snake_body_x = snake_list[body_part][0]
-            snake_body_y = snake_list[body_part][1]
-            if rotate == "RIGHT":
-                screen.blit(snake_body,(snake_body_x - 40,snake_body_y))
-            elif rotate == "DOWN":
-                screen.blit(snake_body,(snake_body_x,snake_body_y - 40))
-            elif rotate == "UP":
-                screen.blit(snake_body,(snake_body_x,snake_body_y + 40))
-            elif rotate == "LEFT":
-                screen.blit(snake_body,(snake_body_x + 40,snake_body_y))
-        #----------------------------------------------
+        #Game over nếu rắn chết(vì đụng trúng bản thân)---------
+        for i in range(len(snake_list)):    
+            body_x = snake_list[i][0]
+            body_y = snake_list[i][1]
+            if (snake_xpos == body_x) and (snake_ypos == body_y) and (i != 0):
+                game_over == True
+            while game_over == True:    
+                message("Hunter Win",red,50,400,width/2)
+                message("Press Q-Quit or C-Play Again",black,25,300,width/2-200)
+                for body_part in range(len(snake_list)):
+                    snake_body_x = snake_list[body_part][0]
+                    snake_body_y = snake_list[body_part][1]
+                    if rotate == "RIGHT":
+                        screen.blit(snake_body,(snake_body_x - 40,snake_body_y))
+                    elif rotate == "DOWN":
+                        screen.blit(snake_body,(snake_body_x,snake_body_y - 40))
+                    elif rotate == "UP":
+                        screen.blit(snake_body,(snake_body_x,snake_body_y + 60))
+                    elif rotate == "LEFT":
+                        screen.blit(snake_body,(snake_body_x + 60,snake_body_y))
+                screen.blit(head_snake_stun, (snake_xpos,snake_ypos))
+                pygame.display.update()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                        pygame.quit()
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_q:
+                            running = False
+                            pygame.quit()
+                        if event.key == pygame.K_c:
+                            main()
         #Ăn táo--------------------------------------------
         if (apple_x - 30 <= snake_xpos <= apple_x + 30) and (apple_y - 30 <= snake_ypos <= apple_y + 30) and (have_apple == True):
             have_apple = False
@@ -175,7 +226,7 @@ def main():
             snake_list.pop()
         #--------------------------------------------------
         #Thả táo-------------------------------------------
-        if time_drop_apple == 100:
+        if time_drop_apple > 50 and have_banana == False:
             time_drop_apple = 0
             apple_x = random.randint(30, 1200)
             apple_y = random.randint(30, 600)
@@ -185,6 +236,25 @@ def main():
         else:
             time_drop_apple += 1
         #-----------------------------------------------------
+        #Ăn chuối--------------------------------------------
+        if (banana_x - 30 <= snake_xpos <= banana_x + 30) and (banana_y - 30 <= snake_ypos <= banana_y + 30) and (have_banana == True):
+            have_banana = False
+            skill_snake += 1
+            banana_x = 0
+            banana_y = 0
+        #Thả chuối-------------------------------------------
+        if time_drop_banana > 500 and have_apple == False:
+            time_drop_banana = 0
+            banana_x = random.randint(30, 1200)
+            banana_y = random.randint(30, 600)
+            have_banana = True
+        if have_banana == True:
+            screen.blit(banana, (banana_x,banana_y))
+        else:
+            time_drop_banana += 1
+        #-----------------------------------------------------
+        print(time_drop_apple,time_drop_banana)
+        print(have_apple,have_banana)
         screen.blit(head_snake, (snake_xpos,snake_ypos))
         pygame.display.update()
             
